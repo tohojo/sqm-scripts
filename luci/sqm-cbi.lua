6e8f0790c39b7d933241c3c01e02c1d7a98871b7
Rodrigo Freire <rfreire@rf.usersys.redhat.com>
2016-03-25 20:07:03 -0300

    Makefile: Move the recipe files to /usr/share
    '/usr/share' seems to be the right place for the scripts. As per `man 7 hier`:
           /usr/share
                  This directory contains subdirectories with specific application
                  data,  that  can  be shared among different architectures of the
                  same OS.  Often one finds  stuff  here  that  used  to  live  in
                  /usr/doc or /usr/lib or /usr/man.

--- luci/sqm-cbi.lua
+++ luci/sqm-cbi.lua
@@ -19,7 +19,7 @@ local net = require "luci.model.network".init()
 local sys = require "luci.sys"
 --local ifaces = net:get_interfaces()
 local ifaces = sys.net:devices()
-local path = "/usr/lib/sqm"
+local path = "/usr/share/sqm"
 local run_path = "/tmp/run/sqm/available_qdiscs"
 
 m = Map("sqm", translate("Smart Queue Management"),
