55e75473c26f9caca70f2e0b61bf22fa30f10a6d
guidosarducci <guidosarducci@users.noreply.github.com>
2017-02-23 23:46:05 -0800

    defaults.sh, run-openwrt.sh: Introduce SHAPER variable and defaults
    Also add SHAPER parameter to platform-specific configuration examples.
    
    Signed-off-by: Tony Ambardar <guidosarducci@users.noreply.github.com>

--- src/run-openwrt.sh
+++ src/run-openwrt.sh
@@ -54,6 +54,7 @@ start_sqm_section() {
     export SHAPER_BURST=$(config_get "$section" shaper_burst)
     export HTB_QUANTUM_FUNCTION=$(config_get "$section" htb_quantum_function)
     export QDISC=$(config_get "$section" qdisc)
+    export SHAPER=$(config_get "$section" shaper)
     export SCRIPT=$(config_get "$section" script)
 
     # The UCI names for these two variables are confusing and should have been
