84ea5db50de042f9e2577181bb070e953a1903e5
guidosarducci <guidosarducci@users.noreply.github.com>
2017-10-30 23:29:40 -0700

    tools/sqm.lua: Provide common Luci SQM helper utilities
    This factors out and defines various functions for reading and parsing
    QDISC capabilities, and some specialized GUI option handling, to be used
    by the other Luci SQM pages.
    
    Define read_caps() to read qdisc and shaper capabilities, and cache them
    within a GUI page load. This uses the helper script 'get-qdisc-caps' to
    retrieve and parse a JSON QDISC capabilities string associated with the
    sections 'script' option. The function attempts to get 'script' using
    formvalue() first before cfgvalue(). This allows a user to press only
    "Save" after selecting a custom script on the 'details' page to reload
    the page and have related QDISC changes reflected in the GUI.
    
    Define the match_caps_pairs() function, which uses a capability filter to
    iterate over a qdisc list or capability list.
    
    Define the function parse_tuple_caps(), which extracts details of 3-tuple
    capabilities, including type, variable values and related descriptive text.
    
    Factor out code for AbstractSection.varianttaboption(), used to set
    up "variant" options that implement complex dependencies within the GUI,
    allowing for varying the displayed option values based on carefully
    defined dependencies.
    
    Provide an AbstractValue.write() replacement that conditionally enables
    the related init script. This is used on the advice of Jo-Philipp Wich
    and following the example of the luci-app-minidlna CBI model, to help
    users by enabling sqm's init script if even a single sqm instance is
    enabled and state is saved ("Save & Apply" or "Save").
    This version additionally checks that the init script is not already
    enabled, to avoid spamming the GUI with notifications.

---
+++ luci/luasrc/tools/sqm.lua
@@ -0,0 +1,144 @@
+--[[
+LuCI - Lua Configuration Interface
+
+Copyright 2017 Tony Ambardar
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
+]]--
+
+module("luci.tools.sqm", package.seeall)
+
+local cbi = require "luci.cbi"
+local i18n = require "luci.i18n"
+local fs = require "nixio.fs"
+local sys = require "luci.sys"
+local util = require "luci.util"
+local json = require "luci.jsonc"
+local qdisc_caps_helper = "/usr/lib/sqm/get-qdisc-caps"
+
+
+-- Provide an AbstractValue.write() replacement that conditionally enables
+-- the related init script. This is used on the advice of Jo-Philipp Wich
+-- and following the example of the luci-app-minidlna CBI model, to help
+-- users by enabling sqm's init script if even a single sqm instance is
+-- enabled and state is saved ("Save & Apply" or "Save"). 
+-- This version additionally checks that the init script is not already
+-- enabled, to avoid spamming the GUI with notifications.
+
+function write_enable_init(cbi_obj, init)
+	assert(util.instanceof(cbi_obj, cbi.AbstractValue),
+		"Object not in AbstractValue class")
+
+	local old_write = cbi_obj.write
+	return function(self, section, value)
+			if value == "1" and not sys.init.enabled(init) then
+				sys.init.enable(init)
+				self.map.message = i18n.translate("The GUI has just enabled "
+				.. "the " .. init .. " initscript on your behalf. Remember to "
+				.. "disable the sqm initscript manually under System Startup "
+				.. "menu in case this change was not wished for.")
+			end
+			return old_write(self, section, value)
+		end
+end
+
+
+-- Implement a "variant" form of taboption to handle complex dependencies,
+-- which allows for varying the displayed option values based on dependencies
+
+function cbi.AbstractSection.varianttaboption(self, vars, ...)
+	assert(type(vars) == "table" and #vars > 0,
+		"Cannot use variant option without table of variants")
+
+	-- The master/template UCI option which is never displayed but
+	-- whose functions we borrow
+	local o = cbi.AbstractSection.taboption(self, ...)
+	o:depends("_nosuchoption", "_nosuchvalue")
+	o.optional = true
+
+	-- Sub-option variants read/write the same master UCI option but at most
+	-- a single variant should be visible. Visibility is determined by the
+	-- variants' depends() calls, and the programmer MUST set these up to be
+	-- mutually exclusive.
+	o.variants = {}
+	local tab, class, opt, desc = select(1,...)
+	for _, v in ipairs(vars) do
+		o.variants[v] = cbi.AbstractSection.taboption(self, tab, class,
+			"_%s_%s" % {v, opt}, desc)
+		o.variants[v].optional = true
+		o.variants[v].cfgvalue = function(s, sc) return o:cfgvalue(sc) end
+		o.variants[v].write = function(s, sc, vl) return o:write(sc, vl) end
+-- Comment is explicit reminder against accidentally removing main option
+--		o.variants[v].remove = function(s, sc) return o:remove(sc) end
+	end
+
+	-- Iterator over (variant,sub-option) pairs
+	o.vpairs = function() return pairs(o.variants) end
+	return o
+end
+
+
+-- Read qdisc and shaper capabilities, and cache within a GUI page load
+
+local caps_cache = {}
+local has_qdisc_helper = fs.access(qdisc_caps_helper)
+
+function read_caps(map, section)
+	assert(has_qdisc_helper,
+		"Helper " .. qdisc_caps_helper .. " missing from sqm-scripts install")
+
+	local option = "script"
+	local cbid = "cbid." .. map.config .. "." .. section .. "." .. option
+	local script = map:formvalue(cbid) or map:get(section, option) or ""
+	local cmd = "%s %s" % {qdisc_caps_helper, script}
+
+	if caps_cache[script] then
+		return caps_cache[script]
+	else
+		local caps = json.parse(sys.exec(cmd)) or {}
+		caps_cache[script] = caps
+		return caps
+	end
+end
+
+
+-- Use capability filter to iterate over qdisc list or capability list
+
+function match_caps_pairs(t,m)
+	local k, v = nil, nil
+	return function ()
+		k, v = next(t, k)
+		while k do
+			if type(v) == "table" then
+				v = table.concat(v," ")
+			end
+			if string.find(v, m) then
+				return k, t[k]
+			end
+			k, v = next(t, k)
+		end
+	end
+end
+
+
+-- Extract details of 3-tuple capabilities, including type, variable values
+-- and related descriptive text
+
+function parse_tuple_caps(q, m)
+	local type_data, qdiscs_with_type = {}, {}
+	for k, c in match_caps_pairs(q, m) do
+		type_data[k] = {}
+		table.insert(qdiscs_with_type, k)
+		for _, s in match_caps_pairs(c, m) do
+			local _, v, d = string.match(s, "(%S+):(%S+):(%S+)")
+			table.insert(type_data[k], { val = v, desc = d:gsub("_", " ") })
+		end
+	end
+	return qdiscs_with_type, type_data
+end
