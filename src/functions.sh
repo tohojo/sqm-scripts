1ed03a6f3156f575490a66c7895015a3affe69f2
Toke Høiland-Jørgensen <toke@toke.dk>
2019-08-17 10:45:48 +0200

    functions.sh: Remove debugging output completely
    Signed-off-by: Toke Høiland-Jørgensen <toke@toke.dk>

--- src/functions.sh
+++ src/functions.sh
@@ -42,7 +42,6 @@ sqm_logger() {
     level_min=${SQM_VERBOSITY_MIN:-$VERBOSITY_SILENT}
     level_max=${SQM_VERBOSITY_MAX:-$VERBOSITY_INFO}
     debug=${SQM_DEBUG:-0}
-    #echo "Level_min: $level_min level_max: $level_max debug: $debug LEVEL: $LEVEL"
 
     if [ "$level_max" -ge "$LEVEL" ] && [ "$level_min" -le "$LEVEL" ] ; then
         if [ "$SQM_SYSLOG" -eq "1" ]; then
